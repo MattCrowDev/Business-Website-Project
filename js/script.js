94bdf7291a658360bea5f4895f0bf16193cbe51c
Matthew Crowell <overthewelldesigns@gmail.com>
2020-03-27 17:10:26 -0700

    was at home so i didnt push instead just locally saving

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openNav() 
+{
+  document.getElementById("mobile__menu").style.width = "100%";
+}
+  
+function closeNav() 
+{
+  document.getElementById("mobile__menu").style.width = "0";
+}
+  
